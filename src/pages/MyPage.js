4826b3c907aacaad2330d4a569aa36fb80da784f
신민제 <sinminje@sinminje-ui-MacBookPro.local>
2021-03-18 22:23:07 +0900

    Add component and css files

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,21 +1,57 @@
 import React from "react";
 import { withRouter } from "react-router";
+import "./css/MyPage.css";
 
 class MyPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.handleClick = this.handleClick.bind(this);
+    this.handleHomeClick = this.handleHomeClick.bind(this);
   }
-  handleClick = () => {
+  handleHomeClick = () => {
     this.props.history.push("/");
   };
 
   render() {
     return (
       <div>
-        <h1>MyPage</h1>
-        <button onClick={() => this.handleClick()}>이동!</button>
+        <nav className="Nav">
+          <button className="home" onClick={() => this.handleHomeClick()}>홈으로</button>
+          <span className="logo">VIMO</span>
+          <span className="logout">logout</span>
+        </nav>
+        <div id="userInfoBox">
+          <div id="imgBox">이미지</div>
+          <div id="userInfo">
+            <div>
+              <span id="username">유저네임</span>
+              <button>edit</button>
+            </div>
+            <div id="email">이메일</div>
+            <div>
+              <span>영화본횟수</span>
+              <span>메모횟수</span>
+              <span>총친구수</span>
+            </div>
+          </div>
+        </div>
+        <div id="myContents">
+          <div className="videoContainer">
+            <div className="video"></div>
+            <div className="video"></div>
+            <div className="video"></div>
+            <div className="video"></div>
+          </div>
+          <div className="videoContainer">
+            <div className="video"></div>
+            <div className="video"></div>
+            <div className="video"></div>
+            <div className="video"></div>
+          </div>
+        </div>
+        <div id="btnContainer">
+          <button id="extend">펼치기</button>
+        </div>
       </div>
     );
   }
